0365a92bd3037a3a9642d0e096b970f471bf2031
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2022-05-19 21:37:09 -0500

    Added GameScreen to  App. Started working on models.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <InitScreen v-if="gameStateStore.gameState == GameState.Init"></InitScreen>
     <MainMenu v-if="gameStateStore.gameState == GameState.MainMenu"></MainMenu>
     <CreditsScreen v-if="gameStateStore.gameState == GameState.Credits"></CreditsScreen>
+    <GameScreen v-show="gameStateStore.gameState == GameState.Gameplay"></GameScreen>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import InitScreen from './components/InitScreen.vue';
 import CreditsScreen from './components/CreditsScreen.vue';
 import { GameState } from './models/GameState';
 import { useGameStateStore } from './stores/game-state';
+import GameScreen from './components/GameScreen.vue';
 
 const gameStateStore = useGameStateStore();
 </script>
