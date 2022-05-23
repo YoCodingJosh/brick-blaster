b9f5cd3a8ea1f6ed37e3f90a45f823768e5bca3e
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2022-05-22 19:19:22 -0500

    smol stuff

--- src/services/internal/LevelGenerationService.ts
+++ src/services/internal/LevelGenerationService.ts
@@ -1,7 +1,9 @@
+import { Level } from "@/models/game/Level";
+
 const MAX_LEVEL_WIDTH = 13;
 
 export abstract class LevelGenerationService {
-  public static generate(level: number) {
-    return;
+  public static generate(level: number): Level {
+    return new Level();
   }
 }
