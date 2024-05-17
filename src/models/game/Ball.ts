5d6a9f48229b79d9e52d5c0ba223851054371e5a
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-05-17 10:16:01 -0500

    Improve ball collision

--- src/models/game/Ball.ts
+++ src/models/game/Ball.ts
@@ -37,11 +37,17 @@ export class Ball {
   public update(deltaTime: number, canvasWidth: number, canvasHeight: number) {
     if (this.isStuck || !this.isActive) return;
 
-    if (this.x + this.dX > canvasWidth - Constants.ballRadius || this.x + this.dX < Constants.ballRadius) {
+    if (
+      this.x + this.dX > canvasWidth - Constants.ballRadius ||
+      this.x + this.dX < Constants.ballRadius
+    ) {
       this.dX = -this.dX;
     }
 
-    if (this.y + this.dY > canvasHeight - Constants.ballRadius || this.y + this.dY < Constants.ballRadius) {
+    if (
+      this.y + this.dY > canvasHeight - Constants.ballRadius ||
+      this.y + this.dY < Constants.ballRadius
+    ) {
       this.dY = -this.dY;
     }
 
@@ -50,33 +56,53 @@ export class Ball {
   }
 
   public handlePlayerCollision(player: Player, deltaTime: number) {
-    if (this.x + Constants.ballRadius >= player.x &&
-      this.x - Constants.ballRadius <= player.x + Constants.playerWidth &&
-      this.y + Constants.ballRadius >= player.y) {
-      // detects where on the paddle the ball has collided
-      // and deflects the ball in the appropriate direction
-      const percentage = (this.x - player.x) / Constants.playerWidth;
-      this.dX = (percentage * this.dXRange) - (this.dXRange / 2);
+    const closestX = Math.max(
+      player.x,
+      Math.min(this.x, player.x + Constants.playerWidth)
+    );
+    const closestY = Math.max(
+      player.y,
+      Math.min(this.y, player.y + Constants.playerHeight)
+    );
 
+    const distanceX = this.x - closestX;
+    const distanceY = this.y - closestY;
+
+    if (
+      distanceX * distanceX + distanceY * distanceY <
+      Constants.ballRadius * Constants.ballRadius
+    ) {
+      const percentage = (this.x - player.x) / Constants.playerWidth;
+      this.dX = percentage * this.dXRange - this.dXRange / 2;
       this.dY = -this.dY * deltaTime;
     }
   }
 
   public handleBrickCollision(brick: Brick, deltaTime: number): boolean {
-    if (this.x - Constants.ballRadius <= brick.x + Constants.brickWidth &&
-      this.x + Constants.ballRadius >= brick.x &&
-      this.y - Constants.ballRadius <= brick.y + Constants.brickHeight &&
-      this.y + Constants.ballRadius >= brick.y) {
-      const percentage = (this.x - brick.x) / Constants.brickWidth;
-      this.dX = (percentage * this.dXRange) - (this.dXRange / 2);
+    const closestX = Math.max(
+      brick.x,
+      Math.min(this.x, brick.x + Constants.brickWidth)
+    );
+    const closestY = Math.max(
+      brick.y,
+      Math.min(this.y, brick.y + Constants.brickHeight)
+    );
 
+    const distanceX = this.x - closestX;
+    const distanceY = this.y - closestY;
+
+    if (
+      distanceX * distanceX + distanceY * distanceY <
+      Constants.ballRadius * Constants.ballRadius
+    ) {
+      const percentage = (this.x - brick.x) / Constants.brickWidth;
+      this.dX = percentage * this.dXRange - this.dXRange / 2;
       this.dY = -this.dY;
 
-      // So we don't accidentally break multiple bricks.
       this.x += this.dX * deltaTime;
       this.y += this.dY * deltaTime;
 
-      return true
+      return true;
     }
 
     return false;
